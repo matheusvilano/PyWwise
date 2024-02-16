1285e5a2fc8409656cc7f406c145191c68fe536c
Matheus Vilano <matheusvilano@icloud.com>
2024-02-15 21:55:46 -0800

    Made changes to the setup script so that it works on Windows.

--- setup.bat
+++ setup.bat
@@ -1,6 +1,12 @@
-python -m venv venv
-source venv/bin/activate  # activate the virtual environment
-pip install -r .requirements
 @echo off
+echo Setting up Python virtual environment...
+
+python -m venv venv
+
+REM Activate the virtual environment on Windows
+venv\Scripts\activate
+
+pip install -r .requirements
+
 echo Done setting up a Python environment.
 pause
