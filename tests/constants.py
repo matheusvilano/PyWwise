bf993a2160a75d2fd73b56a8a388c26525a2d6b6
Matheus Vilano <matheusvilano@icloud.com>
2024-06-08 15:54:11 -0700

    - Added all test objects to `constants.py`, for unit tests and experimentation.

--- tests/constants.py
+++ tests/constants.py
@@ -1,26 +1,191 @@
 from pywwise.types import *
 
-AUDIO_DEVICE__NAME = Name("AudioDevice_Test")
-AUDIO_DEVICE__PATH = ProjectPath(r"\Audio Devices\Default Work Unit\AudioDevice_Test")
-AUDIO_DEVICE__SHORT = ShortID(2188774926)
-AUDIO_DEVICE__GUID = GUID("{B2F34F29-AB61-4028-874C-503751F79CAE}")
+ACOUSTIC_TEXTURE__NAME = Name("AcousticTexture_Test")
+ACOUSTIC_TEXTURE__PATH = ProjectPath(r"\Virtual Acoustics\Default Work Unit\AcousticTexture_Test")
+ACOUSTIC_TEXTURE__SHORT = ShortID(887562500)
+ACOUSTIC_TEXTURE__GUID = GUID("{4B867A8D-20A0-4801-836B-1D74CC131804}")
+
+ACTOR_MIXER__NAME = Name("ActorMixer_Test")
+ACTOR_MIXER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test")
+ACTOR_MIXER__SHORT = ShortID(842083082)
+ACTOR_MIXER__GUID = GUID("{AC6D8219-026E-42B3-9500-78FA814A7338}")
+
+ATTENUATION__NAME = Name("Attenuation_Test")
+ATTENUATION__PATH = ProjectPath(r"\Attenuations\Default Work Unit\Attenuation_Test")
+ATTENUATION__SHORT = ShortID(101771119)
+ATTENUATION__GUID = GUID("{F42E4FE0-7425-47EC-9051-9EE3D77771D2}")
 
 AUDIO_BUS__NAME = Name("AudioBus_Test")
 AUDIO_BUS__PATH = ProjectPath(r"\Master-Mixer Hierarchy\Default Work Unit\Master\AudioBus_Test")
 AUDIO_BUS__SHORT = ShortID(3153391000)
 AUDIO_BUS__GUID = GUID("{A3DB9CC8-E6C9-466D-840A-E1DB87A6029F}")
 
+AUDIO_DEVICE__NAME = Name("AudioDevice_Test")
+AUDIO_DEVICE__PATH = ProjectPath(r"\Audio Devices\Default Work Unit\AudioDevice_Test")
+AUDIO_DEVICE__SHORT = ShortID(2188774926)
+AUDIO_DEVICE__GUID = GUID("{B2F34F29-AB61-4028-874C-503751F79CAE}")
+
 AUX_BUS__NAME = Name("AuxBus_Test")
 AUX_BUS__PATH = ProjectPath(r"\Master-Mixer Hierarchy\Default Work Unit\Master\AuxBus_Test")
 AUX_BUS__SHORT = ShortID(2455487724)
 AUX_BUS__GUID = GUID("{75DBAA74-71BF-4325-A30A-EED68F9A1F3C}")
 
+BLEND_CONTAINER__NAME = Name("BlendContainer_Test")
+BLEND_CONTAINER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test\BlendContainer_Test")
+BLEND_CONTAINER__SHORT = ShortID(571427951)
+BLEND_CONTAINER__GUID = GUID("{1EBC0D80-23AE-4094-85EB-C315E26DF47D}")
+
+CONTROL_SURFACE__NAME = Name("ControlSurface_Test")
+CONTROL_SURFACE__PATH = ProjectPath(r"\Control Surface Sessions\Default Work Unit\ControlSurface_Test")
+CONTROL_SURFACE__SHORT = ShortID(0)
+CONTROL_SURFACE__GUID = GUID("{AC7F5AE7-673C-48C8-A2D2-08B9C5AE3F78}")
+
+CONVERSION_SETTINGS__NAME = Name("ConversionSettings_Test")
+CONVERSION_SETTINGS__PATH = ProjectPath(r"\Conversion Settings\Default Work Unit\ConversionSettings_Test")
+CONVERSION_SETTINGS__SHORT = ShortID(0)
+CONVERSION_SETTINGS__GUID = GUID("{6D1B890C-9826-4384-BF07-C15223E9FB56}")
+
+DIALOGUE_EVENT__NAME = Name("DialogueEvent_Test")
+DIALOGUE_EVENT__PATH = ProjectPath(r"\Dynamic Dialogue\Default Work Unit\DialogueEvent_Test")
+DIALOGUE_EVENT__SHORT = ShortID(3448120196)
+DIALOGUE_EVENT__GUID = GUID("{C85B4D41-CA43-4139-84D1-2CD042624CDD}")
+
+EFFECT__NAME = Name("Effect_Test")
+EFFECT__PATH = ProjectPath(r"\Effects\Default Work Unit\Effect_Test")
+EFFECT__SHORT = ShortID(93280521)
+EFFECT__GUID = GUID("{2BB49B62-E20A-4D05-B960-FFDE3619BA31}")
+
+EVENT__NAME = Name("Event_Test")
+EVENT__PATH = ProjectPath(r"\Events\Default Work Unit\Event_Test")
+EVENT__SHORT = ShortID(1618187162)
+EVENT__GUID = GUID("{E5D47550-1280-497C-BD4B-1A79BB01D7FA}")
+
+GAME_PARAMETER__NAME = Name("GameParameter_Test")
+GAME_PARAMETER__PATH = ProjectPath(r"\Game Parameters\Default Work Unit\GameParameter_Test")
+GAME_PARAMETER__SHORT = ShortID(2811976703)
+GAME_PARAMETER__GUID = GUID("{2F6ABC49-ACB1-443B-A61C-7B47CCF840B5}")
+
+METADATA__NAME = Name("Metadata_Test")
+METADATA__PATH = ProjectPath(r"\Metadata\Default Work Unit\Metadata_Test")
+METADATA__SHORT = ShortID(227449761)
+METADATA__GUID = GUID("{3D9065E1-5A69-4129-B562-096AFDC5F54F}")
+
+MIXING_SESSION__NAME = Name("MixingSession_Test")
+MIXING_SESSION__PATH = ProjectPath(r"\Mixing Sessions\Default Work Unit\MixingSession_Test")
+MIXING_SESSION__SHORT = ShortID(0)
+MIXING_SESSION__GUID = GUID("{6DB2666E-7BAB-4D05-A8C8-FFCFA7725836}")
+
+MODULATOR_LFO__NAME = Name("ModLfo_Test")
+MODULATOR_LFO__PATH = ProjectPath(r"\Modulators\Default Work Unit\ModLfo_Test")
+MODULATOR_LFO__SHORT = ShortID(284738222)
+MODULATOR_LFO__GUID = GUID("{CD42E49D-1F05-4486-8D1D-2B18CE38B224}")
+
+MODULATOR_ENVELOPE__NAME = Name("ModEnvelope_Test")
+MODULATOR_ENVELOPE__PATH = ProjectPath(r"\Modulators\Default Work Unit\ModEnvelope_Test")
+MODULATOR_ENVELOPE__SHORT = ShortID(346576688)
+MODULATOR_ENVELOPE__GUID = GUID("{4A8C3830-50B8-4DEF-ACF1-2882B9D1754A}")
+
+MODULATOR_TIME__NAME = Name("ModTime_Test")
+MODULATOR_TIME__PATH = ProjectPath(r"\Modulators\Default Work Unit\ModTime_Test")
+MODULATOR_TIME__SHORT = ShortID(619105355)
+MODULATOR_TIME__GUID = GUID("{360EF029-357D-4B64-8A7E-306CF23B03B2}")
+
+MUSIC_SWITCH_CONTAINER__NAME = Name("MusicSwitchContainer_Test")
+MUSIC_SWITCH_CONTAINER__PATH = ProjectPath(r"\Interactive Music Hierarchy\Default Work Unit\MusicSwitchContainer_Test")
+MUSIC_SWITCH_CONTAINER__SHORT = ShortID(188990294)
+MUSIC_SWITCH_CONTAINER__GUID = GUID("{306CD70E-1060-4754-B4D9-7A595AD3E502}")
+
+MUSIC_PLAYLIST_CONTAINER__NAME = Name("MusicPlaylistContainer_Test")
+MUSIC_PLAYLIST_CONTAINER__PATH = ProjectPath(r"\Interactive Music Hierarchy\Default Work Unit\MusicSwitchContainer_Test\MusicPlaylistContainer_Test")
+MUSIC_PLAYLIST_CONTAINER__SHORT = ShortID(850509625)
+MUSIC_PLAYLIST_CONTAINER__GUID = GUID("{89E23454-3798-49C0-8C58-D50CF490CCBF}")
+
+MUSIC_SEGMENT__NAME = Name("MusicSegment_Test")
+MUSIC_SEGMENT__PATH = ProjectPath(r"\Interactive Music Hierarchy\Default Work Unit\MusicSwitchContainer_Test\MusicPlaylistContainer_Test\MusicSegment_Test")
+MUSIC_SEGMENT__SHORT = ShortID(398977418)
+MUSIC_SEGMENT__GUID = GUID("{A28758CF-0B29-4FB9-9D7B-AC002F444C02}")
+
+MUSIC_TRACK__NAME = Name("MusicTrack_Test")
+MUSIC_TRACK__PATH = ProjectPath(r"\Interactive Music Hierarchy\Default Work Unit\MusicSwitchContainer_Test\MusicPlaylistContainer_Test\MusicSegment_Test\MusicTrack_Test")
+MUSIC_TRACK__SHORT = ShortID(127101824)
+MUSIC_TRACK__GUID = GUID("{9A86ED1E-D60A-40A3-ADCF-106CFBFF17DB}")
+
+QUERY__NAME = Name("Query_Test")
+QUERY__PATH = ProjectPath(r"\Queries\Default Work Unit\Query_Test")
+QUERY__SHORT = ShortID(0)
+QUERY__GUID = GUID("{7C55BFCF-5BA1-45EA-85EB-910EFCC90A81}")
+
+RANDOM_CONTAINER__NAME = Name("RandomContainer_Test")
+RANDOM_CONTAINER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test\RandomContainer_Test")
+RANDOM_CONTAINER__SHORT = ShortID(173937654)
+RANDOM_CONTAINER__GUID = GUID("{C90BC3F5-AD78-489C-954D-C297EEA62E5D}")
+
+SEQUENCE_CONTAINER__NAME = Name("SequenceContainer_Test")
+SEQUENCE_CONTAINER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test\SequenceContainer_Test")
+SEQUENCE_CONTAINER__SHORT = ShortID(2898814)
+SEQUENCE_CONTAINER__GUID = GUID("{A57159AC-E64B-4E58-9569-7DA1AA422B25}")
+
+SOUND_SFX__NAME = Name("SoundSfx_Test")
+SOUND_SFX__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test\SoundSfx_Test")
+SOUND_SFX__SHORT = ShortID(236491565)
+SOUND_SFX__GUID = GUID("{082B80EF-B825-4CE2-A2B7-46850ECDE6FF}")
+
+SOUND_VOICE__NAME = Name("SoundVoice_Test")
+SOUND_VOICE__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test\SoundVoice_Test")
+SOUND_VOICE__SHORT = ShortID(76119712)
+SOUND_VOICE__GUID = GUID("{39049D64-C7C2-4D6D-848A-6A740F091740}")
+
+SOUNDBANK__NAME = Name("SoundBank_Test")
+SOUNDBANK__PATH = ProjectPath(r"\SoundBanks\Default Work Unit\SoundBank_Test")
+SOUNDBANK__SHORT = ShortID(494357167)
+SOUNDBANK__GUID = GUID("{2DD3590C-441E-4896-A802-77C4EF7EAD5C}")
+
+SOUNDCASTER__NAME = Name("Soundcaster_Test")
+SOUNDCASTER__PATH = ProjectPath(r"\Soundcaster Sessions\Default Work Unit\Soundcaster_Test")
+SOUNDCASTER__SHORT = ShortID(0)
+SOUNDCASTER__GUID = GUID("{58592C4B-4B1D-41E9-B3CA-86E31B8FD7B8}")
+
+STATE_GROUP__NAME = Name("StateGroup_Test")
+STATE_GROUP__PATH = ProjectPath(r"\States\Default Work Unit\StateGroup_Test")
+STATE_GROUP__SHORT = ShortID(4122865920)
+STATE_GROUP__GUID = GUID("{3182E70A-1CD2-4ABD-8652-EEA2E600E4A7}")
+
+STATE_VALUE_NONE__NAME = Name("StateValue_Test_None")
+STATE_VALUE_NONE__PATH = ProjectPath(r"\States\Default Work Unit\StateGroup_Test\StateValue_Test_None")
+STATE_VALUE_NONE__SHORT = ShortID(1632773499)
+STATE_VALUE_NONE__GUID = GUID("{77AA8245-4F64-4941-B1E0-624EEF5FF2C0}")
+
+STATE_VALUE_NOTNONE__NAME = Name("StateValue_Test_NotNone")
+STATE_VALUE_NOTNONE__PATH = ProjectPath(r"\States\Default Work Unit\StateGroup_Test\StateValue_Test_NotNone")
+STATE_VALUE_NOTNONE__SHORT = ShortID(2345074806)
+STATE_VALUE_NOTNONE__GUID = GUID("{6341E917-4A47-4E7A-B896-1EB35D55F005}")
+
+SWITCH_CONTAINER__NAME = Name("SwitchContainer_Test")
+SWITCH_CONTAINER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test\SwitchContainer_Test")
+SWITCH_CONTAINER__SHORT = ShortID(376289075)
+SWITCH_CONTAINER__GUID = GUID("{CF6162C6-E6AA-4C83-B47A-C6689D4A4090}")
+
+SWITCH_GROUP__NAME = Name("SwitchGroup_Test")
+SWITCH_GROUP__PATH = ProjectPath(r"\Switches\Default Work Unit\SwitchGroup_Test")
+SWITCH_GROUP__SHORT = ShortID(412506613)
+SWITCH_GROUP__GUID = GUID("{3163C750-AD28-48C0-A652-DECFDD2840C9}")
+
+SWITCH_VALUE_A__NAME = Name("SwitchValue_Test_A")
+SWITCH_VALUE_A__PATH = ProjectPath(r"\Switches\Default Work Unit\SwitchGroup_Test\SwitchValue_Test_A")
+SWITCH_VALUE_A__SHORT = ShortID(518894723)
+SWITCH_VALUE_A__GUID = GUID("{05D0A833-9359-4E25-8F8E-2E1D413616D7}")
+
+SWITCH_VALUE_B__NAME = Name("SwitchValue_Test_B")
+SWITCH_VALUE_B__PATH = ProjectPath(r"\Switches\Default Work Unit\SwitchGroup_Test\SwitchValue_Test_B")
+SWITCH_VALUE_B__SHORT = ShortID(518894720)
+SWITCH_VALUE_B__GUID = GUID("{3E0B6DA9-8565-43EE-AEF8-1D3E39E6E7C4}")
+
+TRIGGER__NAME = Name("Trigger_Test")
+TRIGGER__PATH = ProjectPath(r"\Triggers\Default Work Unit\Trigger_Test")
+TRIGGER__SHORT = ShortID(151989598)
+TRIGGER__GUID = GUID("{52E7CDB4-A41D-4FA5-AA04-97FD8789DCA6}")
+
 VIRTUAL_FOLDER__NAME = Name("VirtualFolder_Test")
 VIRTUAL_FOLDER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test")
 VIRTUAL_FOLDER__SHORT = ShortID(66897075)
 VIRTUAL_FOLDER__GUID = GUID("{AB482DCB-9BD0-42D3-BA26-302D26770972}")
-
-ACTOR_MIXER__NAME = Name("ActorMixer_Test")
-ACTOR_MIXER__PATH = ProjectPath(r"\Actor-Mixer Hierarchy\Default Work Unit\VirtualFolder_Test\ActorMixer_Test")
-ACTOR_MIXER__SHORT = ShortID(842083082)
-ACTOR_MIXER__GUID = GUID("{AC6D8219-026E-42B3-9500-78FA814A7338}")
