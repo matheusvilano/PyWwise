3d511b69bc6fc6590bf0354b359609e86f799b46
Matheus Vilano <matheusvilano@icloud.com>
2024-06-11 23:43:23 -0700

    - Defined Vector3. - Documented Vector3 and AuxSendValue.

--- pywwise/structs.py
+++ pywwise/structs.py
@@ -3,8 +3,33 @@ from pywwise.types import Name as _Name, GUID as _GUID, ShortID as _ShortID, Gam
 
 
 @dataclasses.dataclass
-class AuxSendValue:
+class Vector3:
+	"""Data-only 3-dimensional vector."""
+	
+	x: float
+	"""X axis."""
+	
+	y: float
+	"""Y axis."""
+	
+	z: float
+	"""Z axis."""
+	
+	@staticmethod
+	def get_zero():
+		""":return: A Vector3 instance with x, y, and z all set to 0.0."""
+		return Vector3(0.0, 0.0, 0.0)
 
+
+@dataclasses.dataclass
+class AuxSendValue:
+	"""Data-only aux send value."""
+	
 	listener: _GameObjectID
+	"""The ID of the associated listener."""
+	
 	aux_bus: _GUID | _Name | _ShortID
+	"""The GUID, name, or short ID of the Aux Bus."""
+	
 	control_value: float
+	"""The intended value."""
