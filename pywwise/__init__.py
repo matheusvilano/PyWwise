986307da43d34c450af07576c3082ed7c1ff8658
Matheus Vilano <matheusvilano@icloud.com>
2024-06-28 16:21:53 -0700

    - Updated docstring for `new`

--- pywwise/__init__.py
+++ pywwise/__init__.py
@@ -6,6 +6,7 @@ from pywwise.ak import Ak as _Ak
 def new(url: _Any = None, allow_exception: bool = False,
         callback_executor: _Any = _SequentialThreadExecutor) -> _Ak:
 	"""
+	Connects to an instance of Wwise.
 	:param url: URL of the Wwise Authoring API WAMP server, defaults to ws://127.0.0.1:8080/waapi
 	:param allow_exception: Allow errors on call and subscribe to throw an exception. Default is False.
 	:param callback_executor: Executor strategy for event callbacks
