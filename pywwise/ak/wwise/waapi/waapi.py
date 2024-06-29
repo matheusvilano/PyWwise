33ae14a8ec96669749a4a4367cad3edab86f6a53
Matheus Vilano <matheusvilano@icloud.com>
2024-06-28 17:17:16 -0700

    Implemented all functions for ak.wwise.waapi

--- pywwise/ak/wwise/waapi/waapi.py
+++ pywwise/ak/wwise/waapi/waapi.py
@@ -11,17 +11,29 @@ class Waapi:
         """
         self._client = client
 
-    def get_functions(self):
+    def get_functions(self) -> tuple[str, ...]:
         """
+        https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_waapi_getfunctions.html \n
         Retrieves the list of functions.
+        :return: A tuple containing the URI of all WAAPI functions.
         """
+        results = self._client.call("ak.wwise.waapi.getFunctions")
+        return tuple(results.get("functions")) if results is not None else ()
 
-    def get_schema(self):
+    def get_schema(self, uri: str) -> dict | None:
         """
+        https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_waapi_getschema.html \n
         Retrieves the JSON schema of a Waapi URI.
+        :param uri: The URI to get the schema for (e.g. "ak.wwise.waapi.getSchema" gets the schema for this function).
+        :return: If the call succeeded, a JSON-like dictionary representing the schema of the specified URI; else, None.
         """
+        return self._client.call("ak.wwise.waapi.getSchema", {"uri": uri})
 
-    def get_topics(self):
+    def get_topics(self) -> tuple[str, ...]:
         """
+        https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_waapi_gettopics.html \n
         Retrieves the list of topics to which a client can subscribe.
+        :return: A tuple containing the URI of all WAAPI topics.
         """
+        results = self._client.call("ak.wwise.waapi.getTopics")
+        return tuple(results.get("topics")) if results is not None else ()
--- pywwise/ak/wwise/wwise.py
+++ pywwise/ak/wwise/wwise.py
@@ -3,6 +3,7 @@ from pywwise.ak.wwise.console import Console as _Console
 from pywwise.ak.wwise.debug import Debug as _Debug
 from pywwise.ak.wwise.core import Core as _Core
 from pywwise.ak.wwise.ui import UI as _UI
+from pywwise.ak.wwise.waapi import Waapi as _Waapi
 
 
 class Wwise:
@@ -20,3 +21,4 @@ class Wwise:
         self.core = _Core(client)
         self.debug = _Debug(client, is_debug_build)
         self.ui = _UI(client)
+        self.waapi = _Waapi(client)
