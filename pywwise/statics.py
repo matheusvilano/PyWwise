0cdc18487be6f3eafc42675bbbd1c650778f689e
Matheus Vilano <matheusvilano@icloud.com>
2024-06-30 12:58:29 -0700

    StringStatics class

--- pywwise/statics.py
+++ pywwise/statics.py
@@ -25,9 +25,90 @@ class EnumStatics(metaclass=StaticMeta):
 		raise ValueError(f"No {_EnumType.__name__} member with value {enum_value}")
 
 
+class StringStatics(metaclass=StaticMeta):
+	"""A static class containing useful utility functions for strings."""
+	
+	@staticmethod
+	def validate_case(text: str) -> bool:
+		"""
+		Checks if the provided string matches one of the common case styles (Snake, Camel, Pascal, etc.). Those are
+		enumerated in `pywwise.enums.ECaseStyle`.
+		:param text: The string to validate.
+		:return: Whether the string is formatted in accordance to one of the common case styles (see
+		`pywwise.enums.ECaseStyle` for a complete list).
+		"""
+		for case in ECaseStyle:
+			if _re_match(case, text):
+				return True
+		return False
+	
+	@staticmethod
+	def to_camel_case(text: str) -> str:
+		"""
+		Converts text to camelCase, to the best possible extent.
+		:param text: The string to convert to camelCase.
+		:return: The string, with the camelCase format.
+		"""
+		if _re_match(ECaseStyle.CAMEL, text):
+			return text
+		tokens = _re_split(r"[-_\s]", text)
+		if tokens == [text]:  # Handle PascalCase or camelCase
+			tokens = _re_findall(r"[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|$)", text)
+		return tokens[0].lower() + ''.join(x.title() for x in tokens[1:])  # Convert to camelCase
+	
+	@staticmethod
+	def to_kebab_case(text: str) -> str:
+		"""
+		Converts text to kebab-case, to the best possible extent.
+		:param text: The string to convert to kebab-case.
+		:return: The string, with the kebab-case format.
+		"""
+		if _re_match(ECaseStyle.KEBAB, text):
+			return text
+		if _re_match(ECaseStyle.PASCAL, text) or _re_match(ECaseStyle.CAMEL, text):
+			tokens = _re_findall(r"[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|$)", text)
+		else:
+			tokens = _re_split(r"[-_\s]", text)
+		return '-'.join([token.lower() for token in tokens])
+	
+	@staticmethod
+	def to_pascal_case(text: str) -> str:
+		"""
+		Converts text to PascalCase, to the best possible extent.
+		:param text: The string to convert to PascalCase.
+		:return: The string, with the PascalCase format.
+		"""
+		if _re_match(ECaseStyle.PASCAL, text):
+			return text
+		as_camel = StringStatics.to_camel_case(text)
+		return f"{as_camel[0].upper()}{as_camel[1:]}"
+	
+	@staticmethod
+	def to_snake_case(text: str) -> str:
+		"""
+		Converts text to snake_case, to the best possible extent.
+		:param text: The string to convert to snake_case.
+		:return: The string, with the snake_case format.
+		"""
+		if _re_match(ECaseStyle.SNAKE, text):
+			return text
+		return StringStatics.to_kebab_case(text).replace('-', '_')
+	
+	@staticmethod
+	def to_upper_case(text: str) -> str:
+		"""
+		Converts text to UPPER_CASE, to the best possible extent.
+		:param text: The string to convert to UPPER_CASE.
+		:return: The string, with the UPPER_CASE format.
+		"""
+		if _re_match(ECaseStyle.UPPER, text):
+			return text
+		return StringStatics.to_snake_case(text).upper()
+
+
 class JsonStatics(metaclass=StaticMeta):
 	"""A static class containing useful utility functions for dictionaries that represent JSON objects."""
-
+	
 	@staticmethod
 	def from_object(obj: _Any) -> dict:
 		"""
