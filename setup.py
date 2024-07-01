0e56ebb8039464984f0fa9c140d3eef5fa8d4af1
Matheus Vilano <matheusvilano@icloud.com>
2024-06-30 17:20:08 -0700

    Simplified and added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
-import setuptools
-from pathlib import Path
+if __name__ == "__main__":
+    from setuptools import find_packages, setup
+    from pathlib import Path
 
-long_description = (Path(__file__).parent/"README.md").read_text()
-
-setuptools.setup(
-		author="Matheus Vilano",
-		author_email="",
-		classifiers=[
-			"Development Status :: 2 - Pre-Alpha",
-			"Intended Audience :: Developers",
-   			"Intended Audience :: Education",
-			"License :: OSI Approved :: MIT License",
-			"Operating System :: OS Independent",
-			"Programming Language :: Python :: 3",
-		],
-		description="A comprehensive WAAPI wrapper - fully documented, with auto-completion in mind.",
-		install_requires=[],
-		keywords="audiokinetic wwise waapi game audio",
-		license="MIT",
-		long_description=long_description,
-		long_description_content_type="text/markdown",  # GitHub-flavored Markdown (GFM)
-		name="pywwise",
-		packages=setuptools.find_packages("pywwise"),
-		package_dir={"": "pywwise"},
-		project_urls={
-			"Author Website": "https://www.matheusvilano.com/",
-			"Git Repository": "https://github.com/matheusvilano/PyWwise.git",
-		},
-		python_requires=">=3.12",
-		url="https://github.com/matheusvilano/PyWwise.git",
-		version="0.0.2",
-)
+    setup(
+        author="Matheus Vilano",
+        author_email="",
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+        ],
+        description="A comprehensive WAAPI wrapper - fully documented, with auto-completion in mind.",
+        install_requires=(Path(__file__).parent / "requirements.txt").read_text().splitlines(),
+        keywords="audiokinetic wwise waapi game audio",
+        license="MIT",
+        long_description=(Path(__file__).parent / "README.md").read_text(),
+        long_description_content_type="text/markdown",  # GitHub-flavored Markdown (GFM)
+        name="pywwise",
+        packages=find_packages("pywwise"),
+        package_dir={"": "pywwise"},
+        project_urls={
+            "Author Website": "https://www.matheusvilano.com/",
+            "Git Repository": "https://github.com/matheusvilano/PyWwise.git",
+        },
+        python_requires=">=3.12",
+        url="https://github.com/matheusvilano/PyWwise.git",
+        version="0.0.2",
+    )
