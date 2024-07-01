6c02cb66c5dc57e05e2e666bcbef9392087e971a
Matheus Vilano <mattvilano@gmail.com>
2024-07-01 04:40:43 -0700

    ak.wwise.core.sound implementation (functions)

--- pywwise/ak/wwise/core/sound.py
+++ pywwise/ak/wwise/core/sound.py
@@ -1,4 +1,7 @@
 from waapi import WaapiClient as _WaapiClient
+from pywwise.enums import EObjectType
+from pywwise.structs import PlatformInfo
+from pywwise.types import GUID, Name, ProjectPath
 
 
 class Sound:
@@ -11,9 +14,20 @@ class Sound:
 		"""
 		self._client = client
 	
-	def get_active_source(self):
+	def set_active_source(self, sound: GUID | Name | ProjectPath, source: GUID | Name | ProjectPath,
+	                      platform: PlatformInfo | Name | GUID = None) -> bool:
 		"""
-		Sets which version of the source is being used for the specified sound. Use
-		`ak.wwise.core.object.get` with the 'activeSource' return option to get the active source of a
-		sound.
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_sound_setactivesource.html \n
+		Sets which version of the source is being used for the specified sound. Use `ak.wwise.core.object.get` with the
+		'activeSource' return option to get the active source of a sound.
+		:param sound: The GUID or the project path of the sound for which to set the active source.
+		:param source: The GUID or the project path of the source.
+		:param platform: The platform on which to execute this operation. By default, the active platform in the Wwise
+		instance will be used.
+		:return: Whether the call succeeded.
 		"""
+		args = {"sound": f"{EObjectType.SOUND}{sound}" if isinstance(sound, Name) else sound,
+		        "source": f"{EObjectType.AUDIO_SOURCE}{source}" if isinstance(source, Name) else source}
+		if platform is not None:
+			args["platform"] = platform
+		return self._client.call("ak.wwise.core.sound.setActiveSource", args) is not None
