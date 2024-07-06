c098e2a9a66223fcce63b0bd0fdaf12cbbb680fa
Matheus Vilano <mattvilano+atlassian@gmail.com>
2024-07-05 17:23:23 -0700

    Implemented ak.wwise.core.log.item_added topic/event

--- pywwise/ak/wwise/core/log.py
+++ pywwise/ak/wwise/core/log.py
@@ -1,4 +1,10 @@
+from simplevent import RefEvent as _RefEvent
 from waapi import WaapiClient as _WaapiClient
+
+from pywwise.decorators import callback
+from pywwise.enums import ELogChannel, ELogSeverity
+from pywwise.statics import EnumStatics
+from pywwise.structs import LogItem
 
 
 class Log:
@@ -10,8 +16,32 @@ class Log:
 		:param client: The WAAPI client to use.
 		"""
 		self._client = client
+		
+		self.item_added = _RefEvent(ELogChannel, LogItem)
+		"""
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_log_itemadded.html
+        \nSent when an item is added to the log. To retrieve the complete log, refer to `ak.wwise.core.log.get`.
+        \n**Event Data**:
+        \n- The channel on which the item was added.
+        \n- The item added to the log.
+        """
+		
+		self._item_added = self._client.subscribe("ak.wwise.core.log.itemAdded", self._on_item_added)
 	
-	def add_tem(self):
+	@callback
+	def _on_item_added(self, **kwargs):
+		"""
+		Callback function for the `itemAdded` event.
+		:param kwargs: The event data.
+		"""
+		channel = EnumStatics.from_value(ELogChannel, kwargs["channel"])
+		item = LogItem(severity=EnumStatics.from_value(ELogSeverity, kwargs["item"]["severity"]),
+		               time=kwargs["item"]["time"],
+		               id=kwargs["item"]["messageId"],
+		               description=kwargs["item"]["message"])
+		self.item_added(channel, item)
+	
+	def add_item(self):
 		"""
 		Adds a new item to the logs on the specified channel.
 		"""
