1ed644bd23824b6112218879c1b5d22d70c436a9
Matheus Vilano <mattvilano+atlassian@gmail.com>
2024-07-07 14:49:41 -0700

    Auto-setup for waapi logger and helper function to set log level

--- pywwise/__init__.py
+++ pywwise/__init__.py
@@ -1,6 +1,9 @@
+from logging import CRITICAL as _LEVEL_CRITICAL, getLogger as _get_logger
 from typing import Any as _Any
 from waapi import SequentialThreadExecutor as _SequentialThreadExecutor
 from pywwise.ak import Ak as _Ak
+
+_get_logger("waapi").setLevel(_LEVEL_CRITICAL)
 
 
 def new(url: _Any = None, allow_exception: bool = False,
@@ -12,3 +15,13 @@ def new(url: _Any = None, allow_exception: bool = False,
 	:param callback_executor: Executor strategy for event callbacks
 	"""
 	return _Ak(url, allow_exception, callback_executor)
+
+
+def set_logging(level: int):
+	"""
+	Sets the WAAPI logger's level. By default, the logger will only log `CRITICAL` errors, which are rare. Use this
+	function if you are experiencing issues with any functions/topics and would like more information from the logger.
+	:param level: The level, represented as an integer. Common values: NOTSET=0, DEBUG=10, INFO=20, WARNING=30,
+				  ERROR=40, CRITICAL=50.
+	"""
+	_get_logger("waapi").setLevel(level)
