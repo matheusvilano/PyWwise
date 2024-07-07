11d4a651970e1414a6ef153283cc7f514daf1570
Matheus Vilano <mattvilano+atlassian@gmail.com>
2024-07-07 14:55:53 -0700

    structs cleanup and documentation formatting

--- pywwise/structs.py
+++ pywwise/structs.py
@@ -256,7 +256,6 @@ class CommandInfo:
 
 @_dataclass
 class LogItem:
-
 	severity: ELogSeverity
 	"""The severity of the message."""
 	
@@ -269,13 +268,12 @@ class LogItem:
 	
 	description: str
 	"""The description of the log item."""
-	
-	
+
+
 @_dataclass
 class SwitchContainerAssignment:
-
 	child: GUID
 	"""The child object of a Switch Container, which is linked (assigned) to a State or Switch value."""
-
+	
 	state_or_switch: GUID
 	"""The State or Switch value to switch the child object is linked (assigned) to."""
