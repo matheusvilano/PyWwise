93867e92b9eb3b6d99ed2e55042f8acdf57ef846
Matheus Vilano <matheusvilano@icloud.com>
2024-07-09 01:04:44 -0700

    Implemented all topics/events for ak.wwise.core.sound_bank

--- pywwise/ak/wwise/core/soundbank.py
+++ pywwise/ak/wwise/core/soundbank.py
@@ -1,5 +1,10 @@
 from waapi import WaapiClient as _WaapiClient
 from simplevent import RefEvent as _RefEvent
+from pywwise.decorators import callback
+from pywwise.enums import ELogSeverity, EReturnOptions
+from pywwise.statics import EnumStatics
+from pywwise.structs import LogItem, SoundBankData, SoundBankGenerationInfo, WwiseObjectInfo
+from pywwise.types import Name
 
 
 class SoundBank:
@@ -11,10 +16,60 @@ class SoundBank:
 		:param client: The WAAPI client to use.
 		"""
 		self._client = client
+		
+		self.generated = _RefEvent(SoundBankGenerationInfo)
+		"""
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_soundbank_generated.html
+		\nSent when a SoundBank is generated. Can multi-trigger during SoundBank generation, per bank and per platform.
+		\n**Event Data**:
+		\n- A SoundBankGenerationInfo instance, which contains information about the generated SoundBank.
+		"""
+		
+		generated_args = {"infoFile": True, "bankData": True, "pluginInfo": True,
+		                  "return": [EReturnOptions.GUID, EReturnOptions.NAME,
+		                             EReturnOptions.TYPE, EReturnOptions.PATH]}
+		
+		self._generated = self._client.subscribe("ak.wwise.core.soundbank.generated",
+		                                         self._on_generated, generated_args)
+		
+		self.generation_done = _RefEvent(tuple[LogItem])
+		"""
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_soundbank_generationdone.html
+		\nSent when all SoundBanks are generated. Do not use to check if `ak.wwise.core.soundbank.generate` has completed.
+		\n**Event Data**:
+		\n- A tuple of LogItems representing the SoundBank generation log. Empty when used in WwiseConsole.
+		"""
+		
+		self._generation_done = self._client.subscribe("ak.wwise.core.soundbank.generationDone",
+		                                               self._on_generation_done)
 	
-		# TODO: implement topics
-		self.generated: _RefEvent
-		self.generation_done: _RefEvent
+	@callback
+	def _on_generated(self, **kwargs):
+		"""
+		Callback function for the `generated` event.
+		:param kwargs: The event data.
+		"""
+		sound_bank = kwargs["soundbank"]
+		sound_bank = WwiseObjectInfo(sound_bank["id"], sound_bank["name"], sound_bank["type"], sound_bank["path"])
+		platform = Name(kwargs["platform"]["name"])
+		language = Name(kwargs["language"]) if kwargs.get("language") is not None else Name.get_null()
+		bank_data = kwargs.get("bankData", dict())
+		bank_data = SoundBankData(bank_data.get("data", ""), bank_data.get("size", 0))
+		banks_info = kwargs.get("bankInfo", list())
+		plugins_info = kwargs.get("pluginInfo", dict())
+		error_message = kwargs.get("error", "")
+		self.generated(SoundBankGenerationInfo(sound_bank, platform, language, bank_data,
+		                                       banks_info, plugins_info, error_message))
+	
+	@callback
+	def _on_generation_done(self, **kwargs):
+		"""
+		Callback function for the `generationDone` event.
+		:param kwargs: The event data.
+		"""
+		items = [LogItem(severity=EnumStatics.from_value(ELogSeverity, item["severity"]), time=item["time"],
+		                 id=item["messageId"], description=item["message"]) for item in kwargs.get("logs", ())]
+		self.generation_done(tuple(items))
 	
 	def convert_external_sources(self):
 		"""
