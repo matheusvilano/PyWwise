afd931d02f5c4175ab4e531407999c76d455b7d1
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2024-08-03 17:05:34 -0700

    Finish Transport.py implementation

--- pywwise/ak/wwise/core/transport.py
+++ pywwise/ak/wwise/core/transport.py
@@ -1,6 +1,7 @@
 from simplevent import RefEvent as _RefEvent
 from waapi import WaapiClient as _WaapiClient
 from pywwise.enums import ETransportExecuteActions
+from pywwise.structs import WwiseTransportObjectInfo
 from pywwise.types import GameObjectID, GUID, Name, ProjectPath
 
 
@@ -73,27 +74,93 @@ class Transport:
         
         return self._client.call("ak.wwise.core.transport.execute", args) is not None
 
-    def get_list(self) -> list():
+    def get_list(self) -> tuple[WwiseTransportObjectInfo, ...]:
         """
         https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_transport_getlist.html \n
         Returns the list of transport objects.
         :return: A list of transport objects.
         """
+        args = {}
+        
+        results = self._client.call("ak.wwise.core.transport.getList", args)
+        results = results.get("list") if results is not None else None
+        if results is None:
+            return ()
+        
+        transport_objects = list[WwiseTransportObjectInfo]
+        
+        for result in results:
+            object = GUID(result["object"])
+            game_object = int(result["gameObject"])
+            transport = int(result["transport"])
+            transport_objects.append(WwiseTransportObjectInfo(object, game_object, transport))
+        
+        return tuple(transport_objects)
     
-    def get_state(self):
+    def get_state(self, transport_id: int) -> tuple[bool, str]:
         """
+        https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_transport_getstate.html \n
         Gets the state of the given transport object.
+        :param transport_id: The transport object ID to be used with all other ak.wwise.core.transport functions. Make
+        sure you create a transport object before calling this function.
+        :return: A tuple made up of a bool and a string message. If the call fails, the bool will be false. If the call
+        succeeds, the bool will be true and the string will represent the current state of the transport object. The
+        return message can either be: Playing, Stopped, Paused
         """
+        
+        if transport_id is None:
+            return False, "No transport object was given."
+        
+        args = {"transport": transport_id}
+        
+        result = self._client.call("ak.wwise.core.transport.getState", args)
+        
+        if result is None:
+            return False, "Function called failed."
+        
+        message = result.get("state")
+        
+        return True, message
     
-    def prepare(self):
+    def prepare(self, object: GUID | ProjectPath | Name) -> bool:
         """
+        https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_transport_prepare.html \n
         Prepare the object and its dependencies for playback. Use this function before calling
         `PostEventSync` or `PostMIDIOnEventSync` from `IAkGlobalPluginContext`.
+        :param object: The name of the object qualified by its type or Short ID in the form of type:name or
+        Global:shortId. Only object types that have globally-unique names or Short Ids are supported.
+        Ex: Event:Play_Sound_01, Global:245489792.
+        :return: True if the call was successful, False otherwise.
         """
+        
+        if object is None:
+            return False
+        
+        args = {"object": object}
+        
+        result = self._client.call("ak.wwise.core.transport.prepare", args)
+        
+        if result is not None:
+            return True
+        else:
+            return False
     
-    def use_originals(self):
+    def use_originals(self, enable_original_files: bool = False) -> bool:
         """
+        https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_transport_useoriginals.html \n
         Sets the Original/Converted transport toggle globally. This allows playing the original or the
         converted sound files.
+        :param enable_original_files: True to enable original files, False to use converted files. If no value is
+        provided, system will default to using converted files.
+        :return: True if the call was successful, False otherwise.
         """
     
+        args = {"enable": enable_original_files}
+        
+        result = self._client.call("ak.wwise.core.transport.useOriginals", args)
+        
+        if result is not None:
+            return True
+        else:
+            return False
+        
