1421a2cd44e56bc2f9fa98ec42a7ef60fb3de01a
Matheus Vilano <matheusvilano@icloud.com>
2024-08-04 23:18:18 -0700

    Preliminary implementation for `copy`, `create`, and `get` functions in ak.wwise.core.object

--- pywwise/ak/wwise/core/object.py
+++ pywwise/ak/wwise/core/object.py
@@ -2,7 +2,7 @@ from typing import Any as _Any
 from waapi import WaapiClient as _WaapiClient, EventHandler as _EventHandler
 from simplevent import RefEvent as _RefEvent
 from pywwise.decorators import callback
-from pywwise.enums import EAttenuationCurveType, EObjectType, EReturnOptions
+from pywwise.enums import EAttenuationCurveType, ENameConflictStrategy, EObjectType, EReturnOptions
 from pywwise.statics import EnumStatics
 from pywwise.structs import WwiseObjectInfo, WwiseObjectWatch
 from pywwise.types import GUID, Name, ProjectPath
@@ -316,19 +316,56 @@ class Object:
 		new = WwiseObjectInfo.from_dict(kwargs["new"])
 		event(obj, old, new)
 	
-	def copy(self):
+	def copy(self, obj: GUID | tuple[EObjectType, Name] | ProjectPath,
+	         parent: GUID | tuple[EObjectType, Name] | ProjectPath,
+	         name_conflict_strategy: ENameConflictStrategy = ENameConflictStrategy.FAIL,
+	         version_control_auto_checkout: bool = True,
+	         version_control_auto_add: bool = True) -> WwiseObjectInfo | None:
 		"""
-		Copies an object to the given parent. Note that if a Work Unit is copied, the operation cannot be
-		undone and the project will be saved.
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_object_copy.html
+		Copies an object to the given parent. Note that if a Work Unit is copied, the operation cannot be undone and
+		the project will be saved.
+		:param obj: The GUID, type and name, or project path of the object to copy.
+		:param parent: The GUID, type and name, or project path of the new object's parent.
+		:param name_conflict_strategy: The strategy to use in case of a name conflict.
+		:param version_control_auto_checkout: Determines if Wwise automatically performs a Checkout source control
+											  operation for affected work units and for the project.
+		:param version_control_auto_add: Determines if Wwise automatically performs an Add source control operation for
+										 affected work units.
+		:return: The new object as a WwiseObjectInfo, or None if the operation failed. Known issue: the WwiseObjectInfo
+				 instance will have invalid values for `path` and `type` attributes.
 		"""
+		args = {"object": f"{obj[0].get_type_name()}:{obj[1]}" if isinstance(obj, tuple) else obj,
+		        "parent": f"{parent[0].get_type_name()}:{parent[1]}" if isinstance(parent, tuple) else parent,
+		        "onNameConflict": name_conflict_strategy,
+		        "autoCheckOutToSourceControl": version_control_auto_checkout,
+		        "autoAddToSourceControl": version_control_auto_add}
+		options = {"return": EReturnOptions.get_defaults()}
+		results = self._client.call("ak.wwise.core.object.copy", args, options=options)
+		return WwiseObjectInfo.from_dict(results["object"])
 	
-	def create(self):
+	def create(self, name: Name | str, etype: EObjectType, parent: GUID | tuple[EObjectType, Name] | ProjectPath,
+	           name_conflict_strategy: ENameConflictStrategy = ENameConflictStrategy.FAIL, notes: str = "",
+	           version_control_auto_checkout: bool = True, platform: Name | GUID = None) -> WwiseObjectInfo:
 		"""
-		Creates an object of type 'type', as a child of 'parent'. Refer to Importing Audio Files and
-		Creating Structures for more information about creating objects. Also refer to
-		`ak.wwise.core.audio.import_files` to import audio files to Wwise. To create Effect or Source
-		plug-ins, use `ak.wwise.core.object.set`, and refer to Wwise Objects Reference for the classId.
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_object_create.html \n
+		Creates an object of type 'type', as a child of 'parent'. Refer to Importing Audio Files and Creating
+		Structures for more information about creating objects. Also refer to `ak.wwise.core.audio.import_files` to
+		import audio files to Wwise. To create Effect or Source plug-ins, use `ak.wwise.core.object.set`, and refer to
+		Wwise Objects Reference for the classId.
 		"""
+		args = dict()
+		args["name"] = name
+		args["type"] = etype.get_type_name()
+		args["parent"] = f"{parent[0].get_type_name()}:{parent[1]}" if isinstance(parent, tuple) else parent
+		args["onNameConflict"] = name_conflict_strategy.value
+		args["notes"] = notes
+		args["autoAddToSourceControl"] = version_control_auto_checkout
+		if platform is not None:
+			args["platform"] = platform
+		
+		results = self._client.call("ak.wwise.core.object.create", args)
+		return WwiseObjectInfo(results["id"], results["name"], etype, ProjectPath.get_null())
 	
 	def delete(self):
 		"""
@@ -341,13 +378,15 @@ class Object:
 		Compares properties and lists of the source object with those in the target object.
 		"""
 	
-	def get(self):
+	def get(self, waql: str):
 		"""
-		Performs a query and returns the data, as specified in the options, for each object in the query
-		result. The query can specify either a 'waql' argument or a 'from' argument with an optional
-		'transform' argument. Refer to Using the Wwise Authoring Query Language (WAQL) or Querying the
-		Wwise Project for more information. Refer to Return Options to learn about options.
+		https://www.audiokinetic.com/en/library/edge/?source=SDK&id=ak_wwise_core_object_get.html \n
+		Performs a query and returns the data, as specified in the options, for each object in the querymresult. The
+		query can specify either a 'waql' argument or a 'from' argument with an optional 'transform' argument. Refer to
+		Using the Wwise Authoring Query Language (WAQL) or Querying the Wwise Project for more information. Refer to
+		Return Options to learn about options.
 		"""
+		return self._client.call("ak.wwise.core.object.get", {"waql": waql})
 	
 	def get_attenuation_curve(self):
 		"""
@@ -361,21 +400,19 @@ class Object:
 	
 	def get_property_info(self):
 		"""
-		Retrieves information about an object property. Note that this function does not return the value
-		of a property. To retrieve the value of a property, refer to `ak.wwise.core.object.get` and Return
-		Options.
+		Retrieves information about an object property. Note that this function does not return the value of a
+		property. To retrieve the value of a property, refer to `ak.wwise.core.object.get` and Return Options.
 		"""
 	
 	def get_types(self):
 		"""
-		Retrieves the list of all object types registered in Wwise's object model. This function returns
-		the equivalent of Wwise Objects Reference.
+		Retrieves the list of all object types registered in Wwise's object model. This function returns the equivalent
+		of Wwise Objects Reference.
 		"""
 	
 	def is_linked(self):
 		"""
-		Indicates whether a property, reference, or object list is bound to a particular platform or to
-		all platforms.
+		Indicates whether a property, reference, or object list is bound to a particular platform or to all platforms.
 		"""
 	
 	def is_property_enabled(self):
@@ -390,18 +427,17 @@ class Object:
 	
 	def paste_properties(self):
 		"""
-		Pastes properties, references and lists from one object to any number of target objects. Only
-		those properties, references and lists which differ between source and target are pasted. Refer
-		to Wwise Objects Reference for more information on the properties, references and lists available
-		on each object type.
+		Pastes properties, references and lists from one object to any number of target objects. Only those properties,
+		references and lists which differ between source and target are pasted. Refer to Wwise Objects Reference for
+		more information on the properties, references and lists available on each object type.
 		"""
 	
 	def set(self):
 		"""
-		Allows for batch processing of the following operations: Object creation in a child hierarchy,
-		Object creation in a list, Setting name, notes, properties and references. Refer to Importing
-		Audio Files and Creating Structures for more information about creating objects. Also refer to
-		`ak.wwise.core.audio.import_files` to import audio files to Wwise.
+		Allows for batch processing of the following operations: Object creation in a child hierarchy, Object creation
+		in a list, Setting name, notes, properties and references. Refer to Importing Audio Files and Creating
+		Structures for more information about creating objects. Also refer to `ak.wwise.core.audio.import_files` to
+		import audio files to Wwise.
 		"""
 	
 	def set_attenuation_curve(self):
@@ -426,32 +462,32 @@ class Object:
 	
 	def set_property(self):
 		"""
-		Sets a property value of an object for a specific platform. Refer to Wwise Objects Reference for
-		more information on the properties available on each object type. Refer to
-		`ak.wwise.core.object.set_reference` to set a reference to an object. Refer to
-		`ak.wwise.core.object.get` to obtain the value of a property for an object.
+		Sets a property value of an object for a specific platform. Refer to Wwise Objects Reference for more
+		information on the properties available on each object type. Refer to `ak.wwise.core.object.set_reference` to
+		set a reference to an object. Refer to `ak.wwise.core.object.get` to obtain the value of a property for an
+		object.
 		"""
 	
 	def set_randomizer(self):
 		"""
-		Sets the randomizer values of a property of an object for a specific platform. Refer to Wwise
-		Objects Reference for more information on the properties available on each object type.
+		Sets the randomizer values of a property of an object for a specific platform. Refer to Wwise Objects Reference
+		for more information on the properties available on each object type.
 		"""
 	
 	def set_reference(self):
 		"""
-		Sets an object's reference value. Refer to Wwise Objects Reference for more information on the
-		references available on each object type.
+		Sets an object's reference value. Refer to Wwise Objects Reference for more information on the references
+		available on each object type.
 		"""
 	
 	def set_state_groups(self):
 		"""
-		Sets the State Group objects associated with an object. Note, this will remove any previously
-		associated State Group.
+		Sets the State Group objects associated with an object. Note, this will remove any previously associated State
+		Group.
 		"""
 	
 	def set_state_properties(self):
 		"""
-		Set the state properties of an object. Note, this will remove any previous state property,
-		including the default ones.
+		Set the state properties of an object. Note, this will remove any previous state property, including the
+		default ones.
 		"""
