f1f274c7739dab9d164656e19963cf900f24ae4f
Matheus Vilano <matheusvilano@icloud.com>
2024-08-15 20:47:48 -0700

    Added long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # Copyright 2024 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from setuptools import find_packages, setup
+from pathlib import Path
+
+
 def main():
-	from setuptools import find_packages, setup
-	from pathlib import Path
-	readme_path = Path(__file__).parent / "README.md"
-	requirements_path = Path(__file__).parent / "requirements.txt"
-	long_description = readme_path.read_text()
-	setup()
+	setup(long_description=(Path(__file__).parent / "README.md").read_text(),
+	      long_description_content_type="text/markdown")
 
 
 if __name__ == "__main__":
