3bfc87bf01418315280a7a0ade51ea3f66ab1ff8
Sawyer King <sawyer.audio@gmail.com>
2025-02-26 23:18:47 -0800

    [#30] Implemented Marker class

--- pywwise/objects/sounds.py
+++ pywwise/objects/sounds.py
@@ -32,8 +32,15 @@ class ExternalSourceFile(WwiseObject):
 
 
 class Marker(WwiseObject):
-    """A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
-    the class represented by `EObjectType.MARKER`."""
+    """
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_marker.html \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.MARKER`.
+    """
+    colour = WwiseProperty[int]("Color", int)
+    label = WwiseProperty[str]("Label", str)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
+    time = WwiseProperty[float]("Time", float)
 
 
 class PluginDataSource(WwiseObject):
