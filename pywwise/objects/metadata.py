39cac871b5b634a9f5f6ff69b63fc775adbd4783
Sawyer King <sawyer.audio@gmail.com>
2025-02-27 15:34:57 -0800

    [#30] Implemented Effect, EffectSlot, Metadata, MidiFileSource, MidiParameter classes

--- pywwise/objects/metadata.py
+++ pywwise/objects/metadata.py
@@ -1,9 +1,20 @@
 # Copyright 2025 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from collections.abc import Sequence as _Sequence
+
 from pywwise.objects.types import WwiseObject
+from pywwise.descriptors import WwiseProperty
+from pywwise.objects.syncs import Rtpc
 
 
 class Metadata(WwiseObject):
-    """A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
-    the class represented by `EObjectType.METADATA`."""
+    """
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_metadata.html \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.METADATA`.
+    """
+    colour = WwiseProperty[int]("Color", int)
+    inclusion = WwiseProperty[bool]("Inclusion", bool)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
+    rtpc = WwiseProperty[_Sequence[Rtpc]]("RTPC", _Sequence[Rtpc])
