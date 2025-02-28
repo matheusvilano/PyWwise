c9c34e722310ab2538a3d74f35588168a120b5be
Sawyer King <sawyer.audio@gmail.com>
2025-02-27 21:09:29 -0800

    [#30] Implemented Language, Platform, Project classes

--- pywwise/objects/project.py
+++ pywwise/objects/project.py
@@ -2,23 +2,97 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from pywwise.objects.types import WwiseObject
+from pywwise.descriptors import WwiseProperty
 
 
 class Language(WwiseObject):
-    """A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
-    the class represented by `EObjectType.LANGUAGE`."""
+    """
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_language.html \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.LANGUAGE`.
+    """
+    volume_offset = WwiseProperty[float]("VolumeOffset", float)
 
 
 class Platform(WwiseObject):
-    """A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
-    the class represented by `EObjectType.PLATFORM`."""
+    """
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_platform.html \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.PLATFORM`.
+    """
 
 
 class Project(WwiseObject):
-    """A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
-    the class represented by `EObjectType.PROJECT`."""
+    """
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_project.html \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.PROJECT`.
+    """
+    always_save_media_ids_file = WwiseProperty[bool]("AlwaysSaveMediaIdsFile", bool)
+    auto_detect_fft_window_size = WwiseProperty[int]("AutoDetectFFTWindowSize", int)
+    auto_detect_threshold_high = WwiseProperty[float]("AutoDetectThresholdHigh", float)
+    auto_detect_threshold_low = WwiseProperty[float]("AutoDetectThresholdLow", float)
+    auto_detect_threshold_medium = WwiseProperty[float]("AutoDetectThresholdMedium", float)
+    auto_sound_bank_all_events = WwiseProperty[bool]("AutoSoundBankAllEvents", bool)
+    auto_sound_bank_enabled = WwiseProperty[bool]("AutoSoundBankEnabled", bool)
+    colour = WwiseProperty[int]("Color", int)
+    comm_port_discovery_broadcast = WwiseProperty[int]("CommPortDiscoveryBroadcast", int)
+    comm_port_discovery_response = WwiseProperty[int]("CommPortDiscoveryResponse", int)
+    comm_serial_post_base = WwiseProperty[int]("CommSerialPostBase", int)
+    copy_loose_streamed_media = WwiseProperty[bool]("CopyLooseStreamedMedia", bool)
+    default_language = WwiseProperty[str]("DefaultLanguage", str)
+    event_action_name_position = WwiseProperty[int]("EventActionNamePosition", int)
+    event_name_case_type = WwiseProperty[int]("EventNameCaseType", int)
+    event_name_modify_case = WwiseProperty[int]("EventNameModifyCase", int)
+    external_sources_input_path = WwiseProperty[str]("ExternalSourcesInputPath", str)
+    external_sources_output_path = WwiseProperty[str]("ExternalSourcesOutputPath", str)
+    filter_behaviour = WwiseProperty[int]("FilterBehavior", int)
+    garbage_collect_wave_analysis = WwiseProperty[bool]("GarbageCollectWaveAnalysis", bool)
+    generate_main_sound_bank = WwiseProperty[bool]("GenerateMainSoundBank", bool)
+    generate_multiple_banks = WwiseProperty[bool]("GenerateMultipleBanks", bool)
+    generate_sound_bank_json = WwiseProperty[bool]("GenerateSoundBankJSON", bool)
+    generate_sound_bank_xml = WwiseProperty[bool]("GenerateSoundBankXML", bool)
+    global_voices_limit = WwiseProperty[int]("GlobalVoicesLimit", int)
+    license_key = WwiseProperty[str]("LicenseKey", str)
+    line_ending = WwiseProperty[int]("LineEnding", int)
+    max_dangerous_virtual_voices = WwiseProperty[int]("MaxDangerousVirtualVoices", int)
+    max_messages_per_message_id = WwiseProperty[int]("MaxMessagesPerMessageId", int)
+    media_auto_bank_sub_folders = WwiseProperty[bool]("MediaAutoBankSubFolders", bool)
+    media_ids_in_single_file = WwiseProperty[bool]("MediaIDsInSingleFile", bool)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
+    remove_unused_generated_files = WwiseProperty[bool]("RemoveUnusedGeneratedFiles", bool)
+    sound_bank_allow_exceeding_sb = WwiseProperty[bool]("SoundBankAllowExceedingSB", bool)
+    sound_bank_definition_file_format = WwiseProperty[int]("SoundBankDefinitionFileFormat", int)
+    sound_bank_generate_definition_file = WwiseProperty[bool]("SoundBankGenerateDefinitionFile", bool)
+    sound_bank_generate_estimated_duration = WwiseProperty[bool]("SoundBankGenerateEstimatedDuration", bool)
+    sound_bank_generate_header_file = WwiseProperty[bool]("SoundBankGenerateHeaderFile", bool)
+    sound_bank_generate_max_attenuation_info = WwiseProperty[bool]("SoundBankGenerateMaxAttenuationInfo", bool)
+    sound_bank_generate_print_colour = WwiseProperty[bool]("SoundBankGeneratePrintColor", bool)
+    sound_bank_generate_print_guid = WwiseProperty[bool]("SoundBankGeneratePrintGUID", bool)
+    sound_bank_generate_print_path = WwiseProperty[bool]("SoundBankGeneratePrintPath", bool)
+    sound_bank_generate_readable_file = WwiseProperty[bool]("SoundBankGenerateReadableFile", bool)
+    sound_bank_header_file_path = WwiseProperty[str]("SoundBankHeaderFilePath", str)
+    sound_bank_include_soundbank_names_strings = WwiseProperty[bool]("SoundBankIncludeSoundbankNamesStrings", bool)
+    sound_bank_paths = WwiseProperty[str]("SoundBankPaths", str)
+    sound_bank_post_generate_custom_cmd_description = WwiseProperty[str]("SoundBankPostGenerateCustomCmdDescription",
+                                                                         str)
+    sound_bank_post_generate_custom_cmd_lines = WwiseProperty[str]("SoundBankPostGenerateCustomCmdLines", str)
+    sound_bank_pre_generate_custom_cmd_description = WwiseProperty[str]("SoundBankPreGenerateCustomCmdDescription", str)
+    sound_bank_pre_generate_custom_cmd_lines = WwiseProperty[str]("SoundBankPreGenerateCustomCmdLines", str)
+    sound_bank_update_audio_files = WwiseProperty[bool]("SoundBankUpdateAudioFiles", bool)
+    source_control_generated_files = WwiseProperty[bool]("SourceControlGeneratedFiles", bool)
+    use_action_name_for_event = WwiseProperty[bool]("UseActionNameForEvent", bool)
+    use_default_language = WwiseProperty[bool]("UseDefaultLanguage", bool)
+    use_max_dangerous_virtual_voices = WwiseProperty[bool]("UseMaxDangerousVirtualVoices", bool)
+    use_max_messages_per_message_id = WwiseProperty[bool]("UseMaxMessagesPerMessageId", bool)
+    volume_threshold = WwiseProperty[float]("VolumeThreshold", float)
+    wwise_console_load_user_settings = WwiseProperty[bool]("WwiseConsoleLoadUserSettings", bool)
+    wwise_version_when_created = WwiseProperty[str]("WwiseVersionWhenCreated", str)
 
 
 class UserProjectSettings(WwiseObject):
-    """A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
-    the class represented by `EObjectType.USER_PROJECT_SETTINGS`."""
+    """
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_userprojectsettings.html \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.USER_PROJECT_SETTINGS`.
+    """
