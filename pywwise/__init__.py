f590870ea4596977c98575ec716a9fe5c445dd6b
Matheus Vilano <matheusvilano@icloud.com>
2025-03-01 15:16:03 -0800

    [#30] Removed `neww_object_connection`. Will stick to using constructors for new `WwiseObject` instances.

--- pywwise/__init__.py
+++ pywwise/__init__.py
@@ -16,24 +16,6 @@ from pywwise.waql import *
 _getLogger("waapi").setLevel(_LEVEL_CRITICAL)
 
 
-def new_object_connection(info: WwiseObjectInfo, ak: WwiseConnection) -> type[WwiseObjectType]:
-    """
-    Uses a `WwiseObjectInfo` instance to create a `WwiseObject` instance. More specifically, the object's type will be
-    a subclass of `WwiseObject` (which can be any Wwise Object Type; examples: `Sound`, `SwitchContainer`, `Event`,
-    etc). `WwiseObject` is very similar to `WwiseObjectInfo in terms of what kind of information they offer, but they
-    are fundamentally different: `WwiseObject` is always up-to-date by querying Wwise for information whenever needed,
-    while `WwiseObjectInfo` uses cached information and therefore is not guaranteed to be up-to-date. It is recommended
-    to always use type hints for the variable storing the new object, to ensure the best linter and auto-completion
-    experience. Example: `sound: Sound = new_object_connection(info, ak)`. Please note that you MUST use the syntax
-    `my_var: MyType` when storing the return value of this function or IntelliSense/auto-completion will NOT work.
-    data after ending your connection will result in errors/exceptions.
-    :param info: The `WwiseObjectInfo` instance to use when creating the new `WwiseObject` instance.
-    :param ak: A connection to Wwise. The connection must be active and running for this object to function properly.
-    :return: A `WwiseObject` instance.
-    """
-    return info.type.get_class()(info.guid, ak)
-
-
 def new_waapi_connection(url: str = "ws://127.0.0.1:8080/waapi", *, allow_exception: bool = False,
                          callback_executor: CallbackExecutor = SequentialThreadExecutor,
                          is_debug_build: bool = False, is_console_instance: bool = False,
