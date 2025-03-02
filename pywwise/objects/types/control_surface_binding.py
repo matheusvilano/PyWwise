cbe06d6805a978678924a46c417a6e49bf15423a
Sawyer King <sawyer.audio@gmail.com>
2025-03-01 23:29:33 -0800

    [#30] Implemented ControlSurfaceBinding, ControlSurfaceBindingGroup, ControlSurfaceSession classes

--- pywwise/objects/types/control_surface_binding.py
+++ pywwise/objects/types/control_surface_binding.py
@@ -1,6 +1,8 @@
 # Copyright 2025 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from pywwise.descriptors import WwiseProperty
+from pywwise.enums import EColour, EControlSurfaceBindingTargetType
 from pywwise.objects.abc import WwiseObject
 
 
@@ -10,3 +12,11 @@ class ControlSurfaceBinding(WwiseObject):
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.CONTROL_SURFACE_BINDING`.
     """
+    colour = WwiseProperty[EColour]("Color", EColour)
+    hardware_controller_key = WwiseProperty[str]("HardwareControllerKey", str)
+    object_index_in_view = WwiseProperty[int]("ObjectIndexInView", int)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
+    target_class_id = WwiseProperty[int]("TargetClassId", int)
+    target_name = WwiseProperty[str]("TargetName", str)
+    target_object = WwiseProperty[WwiseObject]("TargetObject", WwiseObject)
+    target_type = WwiseProperty[EControlSurfaceBindingTargetType]("TargetType", EControlSurfaceBindingTargetType)
