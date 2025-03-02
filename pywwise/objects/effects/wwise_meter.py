36d0037947450e0a78c8083c34b0aac414080f7c
Matheus Vilano <matheusvilano@icloud.com>
2025-03-01 17:04:42 -0800

    [#30] Added and set up effects in `pywwise.effects`. Docstrings are still missing links to AK docs, and `EObjectType` needs to be updated.

---
+++ pywwise/objects/effects/wwise_meter.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Matheus Vilano
+# SPDX-License-Identifier: Apache-2.0
+
+from pywwise.descriptors import WwiseProperty
+from pywwise.objects.abc import WwiseObject
+
+
+class WwiseMeter(WwiseObject):
+    """
+     \n
+    A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
+    the class represented by `EObjectType.WWISE_METER`.
+    """
