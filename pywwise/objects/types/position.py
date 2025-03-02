95e013387b628cdbb2d6bfd29206b1c1a5685f67
Sawyer King <sawyer.audio@gmail.com>
2025-03-01 23:55:48 -0800

    [#30] Implemented Position class

--- pywwise/objects/types/position.py
+++ pywwise/objects/types/position.py
@@ -1,7 +1,12 @@
 # Copyright 2025 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from collections.abc import Sequence as _Sequence
+
+from pywwise.descriptors import WwiseProperty
+from pywwise.enums import EPlayMode, ERandomOrSequence
 from pywwise.objects.abc import WwiseObject
+from pywwise.objects.types.rtpc import Rtpc
 
 
 class Position(WwiseObject):
@@ -10,3 +15,14 @@ class Position(WwiseObject):
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.POSITION`.
     """
+    new_path_for_each_sound = WwiseProperty[bool]("NewPathForEachSound", bool)
+    pan_x = WwiseProperty[float]("PanX", float)
+    pan_y = WwiseProperty[float]("PanY", float)
+    pan_z = WwiseProperty[float]("PanZ", float)
+    play_mechanism_loop = WwiseProperty[bool]("PlayMechanismLoop", bool)
+    play_mechanism_random_or_sequence = WwiseProperty[ERandomOrSequence]("PlayMechanismRandomOrSequence",
+                                                                         ERandomOrSequence)
+    play_mechanism_step_or_continuous = WwiseProperty[EPlayMode]("PlayMechanismStepOrContinuous", EPlayMode)
+    play_mechanism_transition_time = WwiseProperty[float]("PlayMechanismTransitionTime", float)
+    play_mechanism_transition_use = WwiseProperty[bool]("PlayMechanismTransitionUse", bool)
+    rtpc = WwiseProperty[_Sequence[Rtpc]]("RTPC", _Sequence[Rtpc])
