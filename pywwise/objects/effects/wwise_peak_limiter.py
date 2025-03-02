04de24940a748effc515407f1c174fdc434b9eb3
Matheus Vilano <matheusvilano@icloud.com>
2025-03-02 15:32:50 -0800

    [#30] Implemented `WwisePeakLimiter`.

--- pywwise/objects/effects/wwise_peak_limiter.py
+++ pywwise/objects/effects/wwise_peak_limiter.py
@@ -7,7 +7,14 @@ from pywwise.objects.abc import WwiseObject
 
 class WwisePeakLimiter(WwiseObject):
     """
-     \n
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_effect_wwise_peak_limiter.html \n
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.WwisePeakLimiter`.
     """
+    channel_link = WwiseProperty[bool]("ChannelLink", bool)
+    look_ahead = WwiseProperty[float]("LookAhead", float)
+    output_gain = WwiseProperty[float]("OutputGain", float)
+    process_lfe = WwiseProperty[bool]("ProcessLFE", bool)
+    ratio = WwiseProperty[float]("Ratio", float)
+    release_time = WwiseProperty[float]("ReleaseTime", float)
+    threshold = WwiseProperty[float]("Threshold", float)
