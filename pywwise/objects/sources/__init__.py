82154aed09e3c2c58ad5701bccc422fe6efa513c
Matheus Vilano <matheusvilano@icloud.com>
2025-03-01 16:56:44 -0800

    [#30] Added sources in `pywwise.objects`.

---
+++ pywwise/objects/sources/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Matheus Vilano
+# SPDX-License-Identifier: Apache-2.0
+
+from pywwise.objects.sources.impacter import Impacter
+from pywwise.objects.sources.motion_source import MotionSource
+from pywwise.objects.sources.soundseed_air_wind import SoundSeedAirWind
+from pywwise.objects.sources.soundseed_air_woosh import SoundSeedAirWoosh
+from pywwise.objects.sources.soundseed_grain import SoundSeedGrain
+from pywwise.objects.sources.wwise_silence import WwiseSilence
+from pywwise.objects.sources.wwise_synth_one import WwiseSynthOne
+from pywwise.objects.sources.wwise_tone_generator import WwiseToneGenerator
