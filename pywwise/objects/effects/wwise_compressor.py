9b212006d3371bd7bbe2fa13512e56a9c0d82144
Matheus Vilano <matheusvilano@icloud.com>
2025-03-02 14:46:15 -0800

    [#30] Implemented `WwiseCompressor`.

--- pywwise/objects/effects/wwise_compressor.py
+++ pywwise/objects/effects/wwise_compressor.py
@@ -7,7 +7,14 @@ from pywwise.objects.abc import WwiseObject
 
 class WwiseCompressor(WwiseObject):
     """
-     \n
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_effect_wwise_compressor.html \n
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.WWISE_COMPRESSOR`.
     """
+    attack_time = WwiseProperty[float]("AttackTime", float)
+    channel_link = WwiseProperty[bool]("ChannelLink", bool)
+    output_gain = WwiseProperty[float]("OutputGain", float)
+    process_lfe = WwiseProperty[bool]("ProcessLFE", bool)
+    ratio = WwiseProperty[float]("Ratio", float)
+    release_time = WwiseProperty[float]("ReleaseTime", float)
+    threshold = WwiseProperty[float]("Threshold", float)
