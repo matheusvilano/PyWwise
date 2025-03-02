885b322d870c891ffd8683ea7f01c55b52d15d2c
Sawyer King <sawyer.audio@gmail.com>
2025-03-01 23:45:42 -0800

    [#30] Implemented MixingSession class

--- pywwise/objects/types/mixing_session.py
+++ pywwise/objects/types/mixing_session.py
@@ -1,6 +1,8 @@
 # Copyright 2025 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from pywwise.descriptors import WwiseProperty
+from pywwise.enums import EColour
 from pywwise.objects.abc import WwiseObject
 
 
@@ -10,3 +12,5 @@ class MixingSession(WwiseObject):
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.MIXING_SESSION`.
     """
+    colour = WwiseProperty[EColour]("Color", EColour)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
