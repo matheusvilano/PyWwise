81cb1b850981bb929fe88607497673ee8173bb07
Matheus Vilano <matheusvilano@icloud.com>
2025-03-02 15:08:52 -0800

    [#30] Implemented `WwiseGain`.

--- pywwise/objects/effects/wwise_gain.py
+++ pywwise/objects/effects/wwise_gain.py
@@ -7,7 +7,9 @@ from pywwise.objects.abc import WwiseObject
 
 class WwiseGain(WwiseObject):
     """
-     \n
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_effect_wwise_gain.html \n
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.WwiseGain`.
     """
+    full_band_gain = WwiseProperty[float]("FullBandGain", float)
+    lfe_gain = WwiseProperty[float]("LFEGain", float)
