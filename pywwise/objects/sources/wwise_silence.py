4e56ed2a020d556a42d1432f25d670c0c4b3281d
Matheus Vilano <matheusvilano@icloud.com>
2025-03-01 20:12:11 -0800

    [#30] Implemented `WwiseSilence`.

--- pywwise/objects/sources/wwise_silence.py
+++ pywwise/objects/sources/wwise_silence.py
@@ -7,7 +7,10 @@ from pywwise.objects.abc import WwiseObject
 
 class WwiseSilence(WwiseObject):
     """
-     \n
+    https://www.audiokinetic.com/en/library/edge/?source=SDK&id=wwiseobject_source_wwise_silence.html \n
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.SOURCE_PLUGIN`.
     """
+    length = WwiseProperty[float]("Length", float)
+    length_max = WwiseProperty[float]("LengthMax", float)
+    length_min = WwiseProperty[float]("LengthMin", float)
