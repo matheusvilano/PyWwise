6f8ee2c439731b44803f445025fa60e99f15f274
Matheus Vilano <matheusvilano@icloud.com>
2025-03-02 16:58:52 -0800

    [#30] Implemented `MidiFileSource`.

--- pywwise/objects/types/midi_file_source.py
+++ pywwise/objects/types/midi_file_source.py
@@ -10,3 +10,4 @@ class MidiFileSource(WwiseObject):
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.MIDI_FILE_SOURCE`.
     """
+    pass  # There are no properties, currently.
