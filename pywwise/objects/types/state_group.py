de358bb20fb54dfc309d4e1415a5f32aef0a6dd5
Matheus Vilano <matheusvilano@icloud.com>
2025-03-06 13:55:10 -0800

    [#30] RImplemented `StateGroup`.

--- pywwise/objects/types/state_group.py
+++ pywwise/objects/types/state_group.py
@@ -1,6 +1,8 @@
 # Copyright 2025 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from pywwise.descriptors import WwiseProperty
+from pywwise.enums import EColour
 from pywwise.objects.abc import WwiseObject
 
 
@@ -10,3 +12,6 @@ class StateGroup(WwiseObject):
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.STATE_GROUP`.
     """
+    colour = WwiseProperty[EColour]("Color", EColour)
+    default_transition_time = WwiseProperty[float]("DefaultTransitionTime", float)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
