7a4198217bb2c4c219fa687a6210ce4ba8477dd3
Matheus Vilano <matheusvilano@icloud.com>
2025-03-06 13:57:25 -0800

    [#30] RImplemented `SoundcasterSession`.

--- pywwise/objects/types/soundcaster_session.py
+++ pywwise/objects/types/soundcaster_session.py
@@ -1,6 +1,8 @@
 # Copyright 2025 Matheus Vilano
 # SPDX-License-Identifier: Apache-2.0
 
+from pywwise.descriptors import WwiseProperty
+from pywwise.enums import EColour
 from pywwise.objects.abc import WwiseObject
 
 
@@ -10,3 +12,5 @@ class SoundcasterSession(WwiseObject):
     A class serving as an interface for getting/setting properties on Wwise objects. This type specifically targets
     the class represented by `EObjectType.SOUNDCASTER_SESSION`.
     """
+    colour = WwiseProperty[EColour]("Color", EColour)
+    override_colour = WwiseProperty[bool]("OverrideColor", bool)
